e4a0d31f7f27a698c7632dda82ac2fa113e94190
Jon Agüera <jonaguera@gmail.com>
2012-07-25 23:19:57 +0200

    Hasta la seccion https://centrovirtual.educacion.es/Contenidos/UO0001/MSYMFONY2.esp/Mesa_de_trabajo/recursos/manual/unidad6.html#la-acci-n-indexaction

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,8 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new Jazzyweb\AulasMentor\NotasFrontendBundle\JAMNotasFrontendBundle(),
+            new Jazzyweb\AulasMentor\NotasBackendBundle\JAMNotasBackendBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
