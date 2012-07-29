23fd69aceb2e01b50dab66b9643b3e4c57155ca6
Jon Agüera <jonaguera@gmail.com>
2012-07-29 13:04:43 +0200

    Leccion 6

--- src/Jazzyweb/AulasMentor/NotasFrontendBundle/Controller/NotasController.php
+++ src/Jazzyweb/AulasMentor/NotasFrontendBundle/Controller/NotasController.php
@@ -1,186 +1,177 @@
- <?php
+<?php
 
- namespace Jazzyweb\AulasMentor\NotasFrontendBundle\Controller;
+namespace Jazzyweb\AulasMentor\NotasFrontendBundle\Controller;
 
- use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
- class NotasController extends Controller
- {
+class NotasController extends Controller {
 
-     public function indexAction()
-     {
-         $request = $this->getRequest(); // equivalente a $this->get('request');
-         $session = $this->get('session');
+    public function indexAction() {
+        $request = $this->getRequest(); // equivalente a $this->get('request');
+        $session = $this->get('session');
 
-         $ruta = $request->get('_route');
+        $ruta = $request->get('_route');
 
-         switch ($ruta)
-         {
-             case 'jamn_homepage':
+        switch ($ruta) {
+            case 'jamn_homepage':
 
-                 break;
+                break;
 
-             case 'jamn_conetiqueta':
-                 $session->set('busqueda.tipo', 'por_etiqueta');
-                 $session->set('busqueda.valor', $request->get('etiqueta'));
-                 $session->set('nota.seleccionada.id', '');
+            case 'jamn_conetiqueta':
+                $session->set('busqueda.tipo', 'por_etiqueta');
+                $session->set('busqueda.valor', $request->get('etiqueta'));
+                $session->set('nota.seleccionada.id', '');
 
-                 break;
+                break;
 
-             case 'jamn_buscar':
-                 $session->set('busqueda.tipo', 'por_termino');
-                 $session->set('busqueda.valor', $request->get('termino'));
-                 $session->set('nota.seleccionada.id', '');
+            case 'jamn_buscar':
+                $session->set('busqueda.tipo', 'por_termino');
+                $session->set('busqueda.valor', $request->get('termino'));
+                $session->set('nota.seleccionada.id', '');
 
-                 break;
-             case 'jamn_nota':
-                 $session->set('nota.seleccionada.id', $request->get('id'));
-                 break;
-         }
+                break;
+            case 'jamn_nota':
+                $session->set('nota.seleccionada.id', $request->get('id'));
+                break;
+        }
 
-         list($etiquetas, $notas, $nota_seleccionada) = $this->dameEtiquetasYNotas();
+        list($etiquetas, $notas, $nota_seleccionada) = $this->dameEtiquetasYNotas();
 
-         return $this->render('JAMNotasFrontendBundle:Notas:index.html.twig', array(
-                     'etiquetas' => $etiquetas,
-                     'notas' => $notas,
-                     'nota_seleccionada' => $nota_seleccionada,
-                 ));
-     }
+        return $this->render('JAMNotasFrontendBundle:Notas:index.html.twig', array(
+                    'etiquetas' => $etiquetas,
+                    'notas' => $notas,
+                    'nota_seleccionada' => $nota_seleccionada,
+                ));
+    }
 
-     public function nuevaAction()
-     {
-         $request = $this->getRequest();
-         $session = $this->get('session');
+    public function nuevaAction() {
+        $request = $this->getRequest();
+        $session = $this->get('session');
 
-         if ($request->getMethod() == 'POST') {
+        if ($request->getMethod() == 'POST') {
 
-             // si los datos que vienen en la request son buenos guarda la nota
+            // si los datos que vienen en la request son buenos guarda la nota
 
-             $session->setFlash('mensaje', 'Se debería guardar la nota:'
-                     . $request->get('nombre') . '. Como aun no disponemos de un
+            $session->setFlash('mensaje', 'Se debería guardar la nota:'
+                    . $request->get('nombre') . '. Como aun no disponemos de un
                          servicio para persistir los datos, mostramos la nota 1');
 
-             return $this->redirect($this->generateUrl('jamn_nota', array('id' => 1)));
-         }
+            return $this->redirect($this->generateUrl('jamn_nota', array('id' => 1)));
+        }
 
-         list($etiquetas, $notas, $nota_seleccionada) = $this->dameEtiquetasYNotas();
+        list($etiquetas, $notas, $nota_seleccionada) = $this->dameEtiquetasYNotas();
 
-         return $this->render('JAMNotasFrontendBundle:Notas:nueva.html.twig', array(
-                     'etiquetas' => $etiquetas,
-                     'notas' => $notas,
-                     'nota_seleccionada' => $nota_seleccionada,
-                 ));
-     }
+        return $this->render('JAMNotasFrontendBundle:Notas:nueva.html.twig', array(
+                    'etiquetas' => $etiquetas,
+                    'notas' => $notas,
+                    'nota_seleccionada' => $nota_seleccionada,
+                ));
+    }
 
-     public function editarAction()
-     {
-         $request = $this->getRequest();
-         $session = $this->get('session');
+    public function editarAction() {
+        $request = $this->getRequest();
+        $session = $this->get('session');
 
-         // Se recupera la nota que viene en la request para ser editada
+        // Se recupera la nota que viene en la request para ser editada
 
-         $nota = array(
-             'id' => $request->get('id'),
-             'titulo' => 'nota',
-         );
+        $nota = array(
+            'id' => $request->get('id'),
+            'titulo' => 'nota',
+        );
 
 
-         if ($request->getMethod() == 'POST') {
+        if ($request->getMethod() == 'POST') {
 
-             // si los datos que vienen en la request son buenos guarda la nota
+            // si los datos que vienen en la request son buenos guarda la nota
 
-             $session->setFlash('mensaje', 'Se debería editar la nota:'
-                     . $request->get('titulo') .
-                     '. Como aún no disponemos de un servicio para persistir los
+            $session->setFlash('mensaje', 'Se debería editar la nota:'
+                    . $request->get('titulo') .
+                    '. Como aún no disponemos de un servicio para persistir los
                          datos, la nota permanece igual');
 
-             return $this->redirect($this->generateUrl('jamn_nota', array('id' => $request->get('id'))));
-         }
+            return $this->redirect($this->generateUrl('jamn_nota', array('id' => $request->get('id'))));
+        }
 
-         list($etiquetas, $notas, $nota_seleccionada) = $this->dameEtiquetasYNotas();
+        list($etiquetas, $notas, $nota_seleccionada) = $this->dameEtiquetasYNotas();
 
-         return $this->render('JAMNotasFrontendBundle:Notas:editar.html.twig', array(
-                     'etiquetas' => $etiquetas,
-                     'notas' => $notas,
-                     'nota_a_editar' => $nota,
-                 ));
-     }
+        return $this->render('JAMNotasFrontendBundle:Notas:editar.html.twig', array(
+                    'etiquetas' => $etiquetas,
+                    'notas' => $notas,
+                    'nota_a_editar' => $nota,
+                ));
+    }
 
-     public function borrarAction()
-     {
-         $request = $this->getRequest();
-         $session = $this->get('session');
+    public function borrarAction() {
+        $request = $this->getRequest();
+        $session = $this->get('session');
 
-         // borrado de la nota $request->get('id');
+        // borrado de la nota $request->get('id');
 
-         $session->setFlash('mensaje', 'Se debería borrar la nota ' . $request->get('id'));
-         $session->set('nota.seleccionada.id', '');
+        $session->setFlash('mensaje', 'Se debería borrar la nota ' . $request->get('id'));
+        $session->set('nota.seleccionada.id', '');
 
-         return $this->forward('JAMNotasFrontendBundle:Notas:index');
-     }
+        return $this->forward('JAMNotasFrontendBundle:Notas:index');
+    }
 
-     public function miEspacioAction()
-     {
-         $params = 'Los datos de la página de inicio del espacio premium';
-         return $this->render('JAMNotasFrontendBundle:Notas:index', array('params' => $params));
-     }
+    public function miEspacioAction() {
+        $params = 'Los datos de la página de inicio del espacio premium';
+        return $this->render('JAMNotasFrontendBundle:Notas:index', array('params' => $params));
+    }
 
-     public function rssAction()
-     {
+    public function rssAction() {
+        
+    }
 
-     }
+    /**
+     * Función Mock para poder desarrollar y probar la lógica de control.
+     *
+     * La función real que finalmente se implemente, utilizará el filtro almacenado
+     * en la sesión y el modelo para calcular la etiquetas, notas y nota seleccionada
+     * que en cada momento se deban pintar.
+     */
+    protected function dameEtiquetasYNotas() {
+        $session = $this->get('session');
 
-     /**
-      * Función Mock para poder desarrollar y probar la lógica de control.
-      *
-      * La función real que finalmente se implemente, utilizará el filtro almacenado
-      * en la sesión y el modelo para calcular la etiquetas, notas y nota seleccionada
-      * que en cada momento se deban pintar.
-      */
-     protected function dameEtiquetasYNotas()
-     {
-         $session = $this->get('session');
+        $etiquetas = array(
+            array(
+                'id' => 1,
+                'texto' => 'etiqueta 1',
+            ),
+            array(
+                'id' => 2,
+                'texto' => 'etiqueta 2',
+            ),
+            array(
+                'id' => 3,
+                'texto' => 'etiqueta 3',
+            ),
+        );
 
-         $etiquetas = array(
-             array(
-                 'id' => 1,
-                 'texto' => 'etiqueta 1',
-             ),
-             array(
-                 'id' => 2,
-                 'texto' => 'etiqueta 2',
-             ),
-             array(
-                 'id' => 3,
-                 'texto' => 'etiqueta 3',
-             ),
-         );
+        $notas = array(
+            array(
+                'id' => 1,
+                'titulo' => 'nota 1',
+            ),
+            array(
+                'id' => 2,
+                'titulo' => 'nota 2',
+            ),
+            array(
+                'id' => 3,
+                'titulo' => 'nota 3',
+            ),
+        );
 
-         $notas = array(
-             array(
-                 'id' => 1,
-                 'titulo' => 'nota 1',
-             ),
-             array(
-                 'id' => 2,
-                 'titulo' => 'nota 2',
-             ),
-             array(
-                 'id' => 3,
-                 'titulo' => 'nota 3',
-             ),
-         );
+        $nota_selecionada_id = $session->get('nota.seleccionada.id');
+        if (!$nota_selecionada_id) {
+            $nota_selecionada_id = 1;
+        }
 
-         $nota_selecionada_id = $session->get('nota.seleccionada.id');
-         if(!$nota_selecionada_id)
-         {
-             $nota_selecionada_id = 1;
-         }
+        $nota_seleccionada = array(
+            'id' => $nota_selecionada_id,
+            'titulo' => 'nota ' . $nota_selecionada_id,
+        );
+        return array($etiquetas, $notas, $nota_seleccionada);
+    }
 
-         $nota_seleccionada = array(
-             'id' => $nota_selecionada_id,
-             'titulo' => 'nota '. $nota_selecionada_id,
-         );
-         return array($etiquetas, $notas, $nota_seleccionada);
-     }
- }
+}
