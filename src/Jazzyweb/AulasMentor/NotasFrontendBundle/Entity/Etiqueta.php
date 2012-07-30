e46d4c99ef085ebc94cf8e721633ccdccaf497ad
jonaguera <jonaguera@gmail.com>
2012-07-30 14:45:43 +0200

    Finalizo estudio Leccion 7. Inicio ejercicios

---
+++ src/Jazzyweb/AulasMentor/NotasFrontendBundle/Entity/Etiqueta.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace Jazzyweb\AulasMentor\NotasFrontendBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Jazzyweb\AulasMentor\NotasFrontendBundle\Entity\Etiqueta
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Jazzyweb\AulasMentor\NotasFrontendBundle\Entity\EtiquetaRepository")
+ */
+class Etiqueta {
+
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string $texto
+     *
+     * @ORM\Column(name="texto", type="string", length=255)
+     */
+    private $texto;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId() {
+        return $this->id;
+    }
+
+    /**
+     * Set texto
+     *
+     * @param string $texto
+     */
+    public function setTexto($texto) {
+        $this->texto = $texto;
+    }
+
+    /**
+     * Get texto
+     *
+     * @return string 
+     */
+    public function getTexto() {
+        return $this->texto;
+    }
+
+////ASOCIACIONES////
+
+    /**
+     * @ORM\ManyToMany(targetEntity="Nota", mappedBy="etiquetas")
+     */
+    private $notas;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Usuario")
+     */
+    private $usuario;
+
+    ////FIN ASOCIACIONES////
+
+    public function __construct() {
+        $this->notas = new ArrayCollection();
+    }
+
+
+    /**
+     * Add notas
+     *
+     * @param Jazzyweb\AulasMentor\NotasFrontendBundle\Entity\Nota $notas
+     */
+    public function addNota(\Jazzyweb\AulasMentor\NotasFrontendBundle\Entity\Nota $notas)
+    {
+        $this->notas[] = $notas;
+    }
+
+    /**
+     * Get notas
+     *
+     * @return Doctrine\Common\Collections\Collection 
+     */
+    public function getNotas()
+    {
+        return $this->notas;
+    }
+
+    /**
+     * Set usuario
+     *
+     * @param Jazzyweb\AulasMentor\NotasFrontendBundle\Entity\Usuario $usuario
+     */
+    public function setUsuario(\Jazzyweb\AulasMentor\NotasFrontendBundle\Entity\Usuario $usuario)
+    {
+        $this->usuario = $usuario;
+    }
+
+    /**
+     * Get usuario
+     *
+     * @return Jazzyweb\AulasMentor\NotasFrontendBundle\Entity\Usuario 
+     */
+    public function getUsuario()
+    {
+        return $this->usuario;
+    }
+}
